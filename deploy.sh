8e400b15eb42338f0f17ddd30b561ab36c21767e
mityu <mityu.email@gmail.com>
2019-09-08 09:57:20 +0900

    Add alacritty configs

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
+CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}
+
+auto_mkdir (){
+    if [ ! -d $1 ]; then
+        mkdir $1
+        echo "Made directory: $1"
+    fi
+}
+
+
+auto_mkdir $XDG_CONFIG_HOME
 
 echo "Deploying .zshrc"
 ln -sn ${SCRIPT_DIR}/.zshrc ~/.zshrc
 
 
 echo "Deploying .vim"
-if [ ! -d ${HOME}/.vim ]; then
-	mkdir ${HOME}/.vim
-	echo "Made directory ${HOME}/.vim"
-fi
-
+auto_mkdir $HOME/.vim
 for file in ${SCRIPT_DIR}/dot_vim/*
 do
 	ln -snfv $file ${HOME}/.vim
 done
+
+
+echo "Deploying .alacritty"
+ln -snfv ${SCRIPT_DIR}/alacritty $CONFIG_DIR/alacritty
