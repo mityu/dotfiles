3e9238b006936524b24bf53c8aabf4cc22f61e7f
mityu <mityu.mail@gmail.com>
2021-10-12 16:59:38 +0900

    xkeysnail: Update setup script

--- xkeysnail/setup-systemd.sh
+++ xkeysnail/setup-systemd.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 # This script must be executed with `sudo`
 
-groupadd uinput
-useradd -G input,uinput $USER
-echo 'KERNEL=="event*", NAME="input/%k", MODE="660", GROUP="input"' > /etc/udev/rules.d/input.rules
-echo 'KERNEL=="uinput", GROUP="uinput"' > /etc/udev/rules.d/uinput.rules
+# groupadd uinput
+# useradd -G input,uinput $USER
+# echo 'KERNEL=="event*", NAME="input/%k", MODE="660", GROUP="input"' > /etc/udev/rules.d/70-input.rules
+# echo 'KERNEL=="uinput", GROUP="uinput"' > /etc/udev/rules.d/70-uinput.rules
+# 
+# # Make ~/.config/systemd/user/xkeysnail.service
+# if [ ! -d ~/.config/systemd/user ]; then
+#     mkdir -p ~/.config/systemd/user
+# fi
+# cp $(cd $(dirname $0);pwd)/xkeysnail.service ~/.config/systemd/user/
+# systemctl --user enable xkeysnail
 
-# Make ~/.config/systemd/user/xkeysnail.service
-if [ ! -d ~/.config/systemd/user ]; then
-    mkdir -p ~/.config/systemd/user
-fi
-cat > ~/.config/systemd/user/xkeysnail.service <<EOF
-[Unit]
-Description=xkeysnail
-
-[Service]
-KillMode=process
-ExecStart=/usr/bin/xkeysnail /home/mityu/.config/xkeysnail/config.py
-ExecStartPre=/usr/bin/xhost +SI:localuser:root
-Type=simple
-Restart=always
-
-# Update DISPLAY to be the same as `echo $DISPLAY` on your graphical terminal.
-Environment=DISPLAY=:0
-
-[Install]
-WantedBy=default.target
-EOF
-
-systemctl --user enable xkeysnail
+cp $(cd $(dirname $0);pwd)/xkeysnail.service /etc/systemd/system
+systemctl enable xkeysnail
+systemctl start xkeysnail
