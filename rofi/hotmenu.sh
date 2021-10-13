5824cb04e9ddcf7faad732992200f3d5d8da6ff5
mityu <mityu.mail@gmail.com>
2021-10-13 18:12:14 +0900

    rofi: Add new menus

--- rofi/hotmenu.sh
+++ rofi/hotmenu.sh
@@ -2,20 +2,29 @@
 
 if [ $# -eq 0 ]; then
     cat <<EOF
+wezterm
+chromium
 sleep
 shutdown
 reboot
 suspend
 lock
+logout
+restart-xkeysnail
+wezterm-float
 EOF
     exit 0
 fi
 
 case $@ in
-    sleep) systemctl suspend && dm-tool lock ;;
+    sleep) systemctl suspend ;;
     suspend) systemctl suspend ;;
-    shutdown) systemctl poweroff ;;
+    shutdown) systemctl poweroff ;;  # TODO: Confirm?
     lock) dm-tool lock ;;
     reboot) systemctl reboot ;;
+    logout) i3-msg exit ;;  # TODO: Confirm?
+    restart-xkeysnail) systemctl --user restart xkeysnail ;;
+    wezterm-float) i3-msg -q exec wezterm ;;  # TODO: How can I make the new terminal float?
+    *) i3-msg -q exec $@ ;;
 esac
 exit 0
---
+++ rofi/i3wm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    cat <<EOF
+fullscreen-toggle
+floating-toggle
+layout-stacking
+layout-tabbed
+layout-default
+layout-toggle-split
+i3-commands
+EOF
+    exit 0
+fi
+
+case $@ in
+    i3-commands) ;;  # TODO: implement
+    *) echo $@ | sed -e 's/-/ /g' | xargs i3-msg -q ;;
+esac
+exit 0
