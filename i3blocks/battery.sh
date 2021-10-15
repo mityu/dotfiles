a5f6b6dbc8c0e85cbf06a6decfd415c1412b6d30
mityu <mityu.mail@gmail.com>
2021-10-15 16:23:27 +0900

    i3blocks: Update battery icon generator

--- i3blocks/battery.sh
+++ i3blocks/battery.sh
@@ -2,66 +2,53 @@
 
 set -euCo pipefail
 
-function online_icon() {
-  [[ $# -eq 0 ]] && return 1
-  local -ar icons=('' '' '' '' '')
-  local index
-  index=$(expr $1 % ${#icons[@]})
-  echo ${icons[${index}]}
-}
-
-function echo_battery() {
-  [[ $# -eq 1 ]] \
-    && echo -e "\"full_text\": \"$1 \"" \
-    || echo -e "\"full_text\": \"$1 \", \"color\": \"$2\""
-}
-
-function get_battery() {
-  [[ -e '/sys/class/power_supply/BAT1/capacity' ]] \
-    && cat '/sys/class/power_supply/BAT1/capacity' \
-    || echo 0
-}
-
-function online() {
-  [[ -e '/sys/class/power_supply/BAT1/status' && \
-    $(cat /sys/class/power_supply/BAT1/status) == 'Charging' ]] \
-    && return 0
+function is_charging() {
+  if [[ -e '/sys/class/power_supply/BAT1/status' && \
+    $(cat /sys/class/power_supply/BAT1/status) == 'Charging' ]]; then
+    return 0
+  fi
   return 1
 }
 
-function main() {
-  local -Ar \
-    high=( ['value']=79 ['icon']='' ['color']='#fff8dc') \
-    middle=( ['icon']='' ['color']='#fff8dc') \
-    low=( ['value']=31 ['icon']='' ['color']='#ff0000')
-
-  local online_icon='' cnt=0
-  while sleep 1; do
-    cnt=$(expr ${cnt} + 1)
-
-    if online; then
-      online_icon=$(online_icon ${cnt})
-    else
-      [[ -z ${online_icon} && $(expr ${cnt} % 60) -ne 1 ]] \
-        && continue
-      online_icon=''
-    fi
-
-    local battery
-    battery=$(get_battery)
-    if [[ ${battery} -eq 0 ]]; then
-      echo_battery "${online_icon}" "#22ff22"
-    elif [[ ${battery} -gt ${high['value']} ]];then
-      echo_battery \
-        "${online_icon:-${high['icon']}} ${battery}%" ${high['color']}
-    elif [[ ${battery} -lt ${low['value']} ]];then
-      echo_battery \
-        "${online_icon:-${low['icon']}} ${battery}%" ${low['color']}
-    else
-      echo_battery \
-        "${online_icon:-${middle['icon']}} ${battery}%" ${middle['color']}
-    fi
-  done
+function get_battery_rest() {
+  if [[ -e '/sys/class/power_supply/BAT1/capacity' ]]; then
+    cat '/sys/class/power_supply/BAT1/capacity'
+    return 0
+  fi
+  echo 0
 }
 
-main
+function main() {
+    local icon_color=''
+    local icon='Err'
+    local rest=$(get_battery_rest)
+
+    if [[ ${rest} -eq 0 ]]; then
+        icon=''
+    elif [[ ${rest} -lt 21 ]]; then
+        icon=''
+    elif [[ ${rest} -lt 51 ]]; then
+        icon=''
+    elif [[ ${rest} -lt 81 ]]; then
+        icon=''
+    else
+        icon=''
+    fi
+
+    if is_charging; then
+        icon_color='#00ff00'
+    elif [[ ${rest} -lt 31 ]]; then
+        icon_color='#ff3334'
+    fi
+
+    local battery_status="${icon} ${rest}%"
+    if [[ -z "${icon_color}" ]]; then
+        echo -e "\"full_text\": \"${battery_status}\""
+    else
+        echo -e "\"full_text\": \"${battery_status}\", \"color\": \"${icon_color}\""
+    fi
+}
+
+while sleep 1; do
+  main
+done
