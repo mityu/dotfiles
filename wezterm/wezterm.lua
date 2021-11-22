e19fa6067ce1437b58e373e6160754571ca08b97
mityu <mityu.mail@gmail.com>
2021-11-23 07:10:52 +0900

    wezterm: Update configuration
    Set some options and status bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,22 +7,58 @@ else
     font_size = 14
 end
 
+local colors = {
+    background = '#1f1f1f',
+    foreground = '#eaeaea',
+
+    selection_bg = '#666666',
+
+    cursor_fg = '#1f1f1f',
+    cursor_bg = '#fff8dc',
+    cursor_border = '#fff8dc',
+
+    ansi = {'#000000', '#d54e53', '#b9ca4a', '#e6c547', '#7aa6da', '#c397d8', '#70c0ba', '#eaeaea'},
+    brights = {'#666666', '#ff3334', '#9ec400', '#e7c547', '#7aa6da', '#b77ee0', '#54ced6', '#ffffff'}
+}
+
+wezterm.on("update-right-status", function(window, pane)
+  local bat = ""
+  local bat_color = colors.foreground
+  for _, b in ipairs(wezterm.battery_info()) do
+    if b.state_of_charge == 0 then
+        bat = ''
+    elseif b.state_of_charge <= 0.2 then
+        bat = ''
+    elseif b.state_of_charge <= 0.5 then
+        bat = ''
+    elseif b.state_of_charge <= 0.8 then
+        bat = ''
+    else
+        bat = ''
+    end
+    bat = bat .. '  ' .. string.format("%0.f%%", b.state_of_charge * 100)
+
+    if b.state == "Charging" then
+        bat_color = "Green"
+    elseif b.state_of_charge <= 0.3 then
+        bat_color = "Red"
+    end
+  end
+
+  window:set_right_status(wezterm.format({
+      {Foreground = {Color = bat_color}},
+      {Text = bat},
+      {Foreground = {Color = colors.foreground}},
+      {Text = "  " .. wezterm.strftime("%b %-d (%a) %H:%M ")}
+  }));
+end)
+
 return {
+    adjust_window_size_when_changing_font_size = false,
+    exit_behavior = "Close",
     font = wezterm.font('Cica'),
     font_size = font_size,
-    colors = {
-        background = '#1f1f1f',
-        foreground = '#eaeaea',
-
-        selection_bg = '#666666',
-
-        cursor_fg = '#1f1f1f',
-        cursor_bg = '#fff8dc',
-        cursor_border = '#fff8dc',
-
-        ansi = {'#000000', '#d54e53', '#b9ca4a', '#e6c547', '#7aa6da', '#c397d8', '#70c0ba', '#eaeaea'},
-        brights = {'#666666', '#ff3334', '#9ec400', '#e7c547', '#7aa6da', '#b77ee0', '#54ced6', '#ffffff'}
-    },
+    colors = colors,
 
     disable_default_key_bindings = true,
     leader = {key = 'p', mods = 'SUPER'},
