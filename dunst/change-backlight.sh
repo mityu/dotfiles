4a5d988fc6f1ec1bfb5a22d4962d8be3d0a6083a
mityu <mityu.mail@gmail.com>
2021-11-26 16:38:13 +0900

    i3, dunst: Show display brightness changes

---
+++ dunst/change-backlight.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+xbacklight $*
+
+if ! which dunstify &> /dev/null; then
+    exit 0
+fi
+brightness=$(xbacklight -get | awk '{print int($1)}')
+dunstify -a 'DisplayBrightness' -t 2000 -u low -i display -h int:value:$brightness \
+    -h string:x-dunst-stack-tag:displayBrightness "Brightness:${brightness}%"
