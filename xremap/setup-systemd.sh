7d7dbc7d3819afb9586179a98364ced3dafc75e0
mityu <mityu.mail@gmail.com>
2022-04-08 14:26:56 +0900

    xremap: Update setup script

--- xremap/setup-systemd.sh
+++ xremap/setup-systemd.sh
@@ -4,7 +4,8 @@ if [ ! -d ~/.config/systemd/user ]; then
     mkdir -p ~/.config/systemd/user
 fi
 cp $(cd $(dirname $0);pwd)/xremap.service ~/.config/systemd/user/
+systemctl --user daemon-reload
 systemctl --user disable xremap.service
 systemctl --user enable xremap.service
 echo 'Please reboot your computer, or do:'
-echo '\t$ systemctl --user start xremap.service'
+echo '  $ systemctl --user start xremap.service'
