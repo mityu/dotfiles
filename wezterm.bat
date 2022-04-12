117956d9606fc6100cddd1d9a74711c793c19716
mityu <mityu.mail@gmail.com>
2022-04-12 22:53:37 +0900

    wezterm: Add launcher script for Windows

---
+++ wezterm.bat
@@ -0,0 +1,4 @@
+set PATH=%USERPROFILE%\vim\src;%USERPROFILE%\go\bin;C:\msys64\mingw64\bin;C:\msys64\usr\bin;C:\msys64\usr\local\bin;%PATH%;
+set GOROOT=C:\msys64\mingw64\lib\go
+cd %USERPROFILE%
+start %USERPROFILE%\WezTerm\wezterm-gui
