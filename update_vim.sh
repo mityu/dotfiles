a12ece11157981d7067bba875637f25a84a5f5a9
mityu <mityu.mail@gmail.com>
2022-04-18 21:09:05 +0900

    Fix update_vim.sh fails to recognize updates

--- update_vim.sh
+++ update_vim.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -x
 cd $1
 HASH=$(git rev-parse HEAD)
-git fetch
+git pull
 if [ $HASH == $(git rev-parse HEAD) ]; then
     exit 0
 fi
-git merge FETCH_HEAD
 make -j4 && make install
 if [ $? -ne 0 ]; then
     make distclean && make -j4 && make install
