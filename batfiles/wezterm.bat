ab1502880fbe80f7d8e98c511f74fda0a014aca7
mityu <mityu.mail@gmail.com>
2022-04-19 12:34:46 +0900

    Set %EDITOR% and %GIT_EDITOR%

--- batfiles/wezterm.bat
+++ batfiles/wezterm.bat
@@ -1,4 +1,6 @@
 set PATH=%~dp0;%USERPROFILE%\vim\src;%USERPROFILE%\go\bin;C:\msys64\mingw64\bin;C:\msys64\usr\bin;C:\msys64\usr\local\bin;%PATH%;
 set GOROOT=C:\msys64\mingw64\lib\go
+set EDITOR=vim
+set GIT_EDITOR=vim
 cd %USERPROFILE%
 start %USERPROFILE%\WezTerm\wezterm-gui
