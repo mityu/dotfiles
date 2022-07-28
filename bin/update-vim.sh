b9f453bd48860c3d6e6394b15232cf0ae7487311
mityu <mityu.mail@gmail.com>
2022-07-28 22:49:43 +0900

    vim: Add force build option for update-vim.sh

--- bin/update-vim.sh
+++ bin/update-vim.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -x
+FORCEBUILD=false
+if [[ "$1" == "--force" || "$1" == "-f" ]]; then
+    FORCEBUILD=true
+fi
 if [ ! -d "$HOME/.cache" ]; then
     mkdir ~/.cache
 fi
 if [ ! -d "$HOME/.cache/vimbuild" ]; then
     git clone https://github.com/vim/vim ~/.cache/vimbuild
-    HASH=null
+    FORCEBUILD=true
 fi
 cd ~/.cache/vimbuild
-: "${HASH:=$(git rev-parse HEAD)}"
+HASH=$(git rev-parse HEAD)
 git pull
-if [ $HASH == $(git rev-parse HEAD) ]; then
-    exit 0
-fi
-make -j4 && make install
-if [ $? -ne 0 ]; then
-    make distclean && make -j4 && make install
+if [[ $HASH != $(git rev-parse HEAD) ]] || $FORCEBUILD; then
+    (! $FORCEBUILD && make -j4) || (make distclean && make -j4) && make install
 fi
