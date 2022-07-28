847d35c44f3f04daf382084546248bcf3616bfa2
mityu <mityu.mail@gmail.com>
2022-07-29 02:36:45 +0900

    MSYS2: Improve update script

--- batfiles/update-softwares.bat
+++ batfiles/update-softwares.bat
@@ -1,12 +1,17 @@
 : <<'EOL'
 @echo off
 setlocal
-set MSYS2=C:\msys64\msys2_shell.cmd
+where /Q cygpath
+if %ERRORLEVEL% == 0 (
+    for /f "usebackq" %%A in (`cygpath -w /msys2_shell.cmd`) do set MSYS2=%%A
+) else (
+    set MSYS2=C:\msys64\msys2_shell.cmd
+)
 if not exist "%MSYS2%" (
     echo %MSYS2% not found.
     goto :EOF
 )
-%MSYS2% -mingw64 -no-start %~dpnx0
+%MSYS2% -mingw64 -no-start -defterm %~dpnx0
 goto :EOF
 EOL
 pacman -Syyu --noconfirm || read -p 'Error while updating softwares...'
