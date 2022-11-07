2aee9450de20475c2d82b319e3f033366e03fdc9
mityu <mityu.mail@gmail.com>
2022-11-07 11:35:26 +0900

    WSL: Add usage for wslterm.bat

--- batfiles/wslterm.bat
+++ batfiles/wslterm.bat
@@ -6,6 +6,13 @@ set A=%A:"=%
 if "%A%" == "%A:/c=%" (
     REM When launched from terminal.
     REM Ref: https://gist.github.com/seraphy/0fc14023b40935ed925021e64ebaf7b2
+
+    if "%1" == "--help" (
+        goto :usage
+    ) else if "%1" == "-h" (
+        goto :usage
+    )
+
     for %%o in (start,new-window) do (
         if "%1" == "%%o" (
             goto :start
@@ -19,6 +26,14 @@ if "%A%" == "%A:/c=%" (
 ) else (
     goto :start
 )
+
+:usage
+echo Usage: %0 [start^|new-window^|--help]
+echo   [No arguments]   Start WSL here
+echo   start            Start WSL in a new window
+echo   new-window       Same above
+echo   --help           Show this help
+goto :EOF
 
 :nostart
 wsl.exe -d Ubuntu-18.04
