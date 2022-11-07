649532bebb5504d4ef5cdb8086faac286d498362
mityu <mityu.mail@gmail.com>
2022-11-07 11:44:07 +0900

    Windows: Add usage for bashterm.bat

--- batfiles/bashterm.bat
+++ batfiles/bashterm.bat
@@ -5,6 +5,13 @@ set A=%A:"=%
 if "%A%" == "%A:/c=%" (
     REM When launched from terminal.
     REM Ref: https://gist.github.com/seraphy/0fc14023b40935ed925021e64ebaf7b2
+
+    if "%1" == "--help" (
+        goto :usage
+    ) else if "%1" == "-h" (
+        goto :usage
+    )
+
     for %%o in (start,new-window) do (
         if "%1" == "%%o" (
             goto :start
@@ -18,6 +25,16 @@ if "%A%" == "%A:/c=%" (
 ) else (
     goto :start
 )
+
+:usage
+echo Batchfile to launch msys2 bash ^(mingw64^).
+echo
+echo Usage: %0 [start^|new-window^|--help]
+echo   [No arguments]   Start bash here
+echo   start            Start bash in a new window
+echo   new-window       Same above
+echo   --help           Show this help
+goto :EOF
 
 :nostart
 C:\msys64\msys2_shell.cmd -mingw64 -defterm -no-start -use-full-path
