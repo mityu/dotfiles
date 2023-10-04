dda002949a97517f8b21496397d5156f7273612f
mityu <mityu.mail@gmail.com>
2023-10-04 10:27:01 +0900

    vim: Use Tapi_drop function to implement drop command on Windows

--- batfiles/drop.bat
+++ batfiles/drop.bat
@@ -1,3 +1,3 @@
 @echo off
 if "%VIM_TERMINAL%" == "" goto :EOF
-vim --servername %VIM_SERVERNAME% --remote-send "<Cmd>split %~dp0%1<CR>"
+vim --servername %VIM_SERVERNAME% --remote-send "<Cmd>call Tapi_drop(0, ['%cd%', '%1'])<CR>"
