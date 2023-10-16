d0f692e1160f780def28ceec97efd8b51fe2a9e9
mityu <mityu.mail@gmail.com>
2023-10-16 14:09:43 +0900

    cmd.exe: Fix "dotfiles cd" does not change cwd

--- batfiles/dotfiles.bat
+++ batfiles/dotfiles.bat
@@ -25,5 +25,14 @@ git -C %DOTFILES% pull
 goto :EOF
 
 :cd
-cd %DOTFILES%
+REM HACK: Before "endlocal", changes of current working directory will be
+REM discarded and we need to do "endlocal" before cd, but after "endlocal"
+REM DOTFILES variable will be cleared to previous state and is no longer
+REM available when do chdir.  In order to make it compatible use of "setlocal"
+REM and chdir, use "for" statement to pass contents of DOTFILES variable for a
+REM temporal variable, disable "setlocal" functionality, and then do chdir.
+for %%d in (%DOTFILES%) do (
+    endlocal
+    cd %%d
+)
 goto :EOF
