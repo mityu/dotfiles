f3e1e12ae5505b2c3d8af91c985c6270eee97c2c
mityu <mityu.mail@gmail.com>
2024-02-12 02:45:38 +0900

    nvim: Add some colorschemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,5 +7,40 @@ return {
     config = function()
       vim.cmd 'colorscheme rose-pine'
     end,
-  }
+  },
+  {
+    'ribru17/bamboo.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('bamboo').setup({
+        dim_inactive = true,
+        code_style = {
+          comments = 'none',
+          conditionals = 'none',
+          keywords = 'none',
+          functions = 'none',
+          namespaces = 'none',
+          parameters = 'none',
+          strings = 'none',
+          variables = 'none',
+        },
+      })
+      require('bamboo').load()
+    end,
+  },
+  {
+    'https://github.com/navarasu/onedark.nvim',
+    lazy = true,
+    priority = 1000,
+    config = function()
+      local onedark = require('onedark')
+      onedark.setup({
+        style = 'warmer',
+        code_style = {
+          comments = 'none',
+        },
+      })
+    end,
+  },
 }
