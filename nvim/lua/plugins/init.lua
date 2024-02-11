628093df7a604b609d3caf9d5e56d2bfc1e020b9
mityu <mityu.mail@gmail.com>
2024-02-12 02:36:32 +0900

    nvim: Remove plugins/init.lua
    It's no longer used.

--- nvim/lua/plugins/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- 'https://github.com/ellisonleao/gruvbox.nvim',
-  -- 'https://github.com/itchyny/vim-cursorword',
-  -- 'https://github.com/kamichidu/vim-textobj-function-go',
-  -- 'https://github.com/kana/vim-operator-user',
-  -- 'https://github.com/kana/vim-textobj-user',
-  -- 'https://github.com/sainnhe/edge',
-}
