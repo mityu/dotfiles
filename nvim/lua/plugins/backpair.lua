2c0a8d01ac5cc4eb1e7922a72b0e4ece0b2d5fee
mityu <mityu.mail@gmail.com>
2024-03-21 05:47:49 +0900

    nvim: Add backpair.vim

---
+++ nvim/lua/plugins/backpair.lua
@@ -0,0 +1,22 @@
+return {
+  'https://github.com/mityu/vim-backpair',
+  event = { 'InsertEnter' },
+  config = function()
+    vim.fn['backpair#enable']()
+    vim.fn['backpair#add_pair']('(', ')')
+    vim.fn['backpair#add_pair']('[', ']', { skip_if_ongoing = { '[[]' } })
+    vim.fn['backpair#add_pair']('<', '>')
+    vim.fn['backpair#add_pair']('{', '}')
+    vim.fn['backpair#add_pair']('"', '"')
+    vim.fn['backpair#add_pair']("'", "'")
+    vim.fn['backpair#add_pair']([[\(]], [[\)]])
+    vim.fn['backpair#add_pair']([[\%(]], [[\)]])
+    vim.fn['backpair#add_pair']('[[', ']]',
+      {enable_filetypes = { 'lua', 'sh', 'bash', 'zsh', 'toml' }})
+    vim.fn['backpair#add_pair']('`', '`', {
+      condition = function()
+        return vim.fn.trim(vim.fn.getline('.')) ~= '``'
+      end,
+    })
+  end,
+}
