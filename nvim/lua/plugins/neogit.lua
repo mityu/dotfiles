4a8de953b8583afdf35e7612e6706f5fa0e9c72f
mityu <mityu.mail@gmail.com>
2024-05-28 20:39:57 +0900

    nvim: Use neogit of master branch

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -4,7 +4,6 @@ return {
     'https://github.com/nvim-lua/plenary.nvim',
     'https://github.com/nvim-telescope/telescope.nvim',
   },
-  branch = 'nightly',
   cmd = 'Neogit',
   keys = { '<Space>g' },
   config = function()
