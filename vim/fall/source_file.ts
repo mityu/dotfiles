3c91a14af1f54c0395fc4cb2d594ab4f8debbde7
mityu <mityu.mail@gmail.com>
2025-03-10 05:34:27 +0900

    vim: Add gitrepo file source for fall.vim

--- vim/fall/source_file.ts
+++ vim/fall/source_file.ts
@@ -1,9 +1,13 @@
 import type { Denops } from "jsr:@denops/std@^7.4.0";
+import { collect } from "jsr:@denops/std@^7.4.0/batch";
+import { expand, getcwd } from "jsr:@denops/std@^7.4.0/function";
 import { ensure } from "jsr:@core/unknownutil@^4.3.0/ensure";
 import { isString } from "jsr:@core/unknownutil@^4.3.0/is/string";
-import { composeSources } from "jsr:@vim-fall/std@^0.11.0/source";
+import { composeSources, defineSource } from "jsr:@vim-fall/std@^0.11.0/source";
 import { bindSourceArgs } from "jsr:@vim-fall/std@^0.11.0";
 import { join } from "jsr:@std/path@^1.0.0/join";
+import { dirname } from "jsr:@std/path@^1.0.0/unstable-dirname";
+import { exists } from "jsr:@std/fs@^1.0.0/exists";
 import {
   file as fileBase,
   type FileOptions,
@@ -15,6 +19,21 @@ const callVim = async (
   ...args: unknown[]
 ): Promise<string> => {
   return ensure(await denops.call(fn, ...args), isString);
+};
+
+const findDir = async (name: string, basePath: string) => {
+  name = name.replace(/\/$/, "");
+  for (;;) {
+    if (await exists(join(basePath, name), { isDirectory: true })) {
+      return basePath;
+    }
+
+    const nextPath = dirname(basePath);
+    if (nextPath === basePath) {
+      return undefined;
+    }
+    basePath = nextPath;
+  }
 };
 
 export const file = fileBase;
@@ -70,4 +89,30 @@ export const localpack = (options: FileOptions = {}) => {
       ) => [join(await callVim(denops, "FallGetStdpath", "localpack"), "opt")],
     ),
   );
+};
+
+/**
+ * Automatically find the git repository top from the current buffer's file
+ * name or the current working directory, and list the files in the directory.
+ */
+export const gitrepo = (options: FileOptions = {}) => {
+  const source = file(options);
+  return defineSource(async function* (denops, { args }, options) {
+    const basePath = await (async () => {
+      if (args.length !== 0) {
+        return args[0];
+      } else {
+        const [curfile, cwd] = await collect(
+          denops,
+          (denops) => [expand(denops, "%:p"), getcwd(denops, 0)],
+        ) as [string, string];
+        return curfile !== "" ? curfile : cwd;
+      }
+    })();
+
+    const path = await findDir(".git", basePath);
+    if (path) {
+      yield* source.collect(denops, { args: [path] }, options);
+    }
+  });
 };
