882077203497dad1064ad887f9d63491491d28a8
mityu <mityu.mail@gmail.com>
2025-03-29 07:37:55 +0900

    nix: Add firefox

--- nix/flake.nix
+++ nix/flake.nix
@@ -7,6 +7,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     xremap = {
