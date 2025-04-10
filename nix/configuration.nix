5a1823160c961ee3c1f0ff65d6ca7022d63c7b82
mityu <mityu.mail@gmail.com>
2025-04-11 02:08:18 +0900

    nix: Fix font config
    Monospace font is wrongly set.

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -152,10 +152,10 @@ in
     fontDir.enable = true;
     fontconfig = {
       defaultFonts = {
-        serif = ["Noto Serif CJK JP" "Noto Color Emoji"];
-        sansSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
-        monospace = ["JetBrains Nerd Font" "Noto Color Emoji"];
-        emoji = ["Noto Color Emoji"];
+        serif = [ "Noto Serif CJK JP" "Noto Color Emoji" ];
+        sansSerif = [ "Noto Sans CJK JP" "Noto Color Emoji" ];
+        monospace = [ "Noto Sans Mono CJK JP" "Noto Color Emoji" ];
+        emoji = [ "Noto Color Emoji" ];
       };
     };
   };
