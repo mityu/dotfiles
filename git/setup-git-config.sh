71c170e6f3cc77dc03e3dfa028323e8f84ce942e
mityu <mityu.mail@gmail.com>
2025-05-24 01:02:45 +0900

    git: Update git setup script

--- git/setup-git-config.sh
+++ git/setup-git-config.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
+exit_status=0
+
 set-config() {
-	if git config get --global $1 &> /dev/null; then
+	local overwrite=false
+	if [[ "$1" == '-f' ]]; then
+		overwrite=true
+		shift
+	fi
+
+	if ! $overwrite && git config get --global $1 &> /dev/null; then
 		echo "Skipped '$1=$2': Already set to '$(git config get --global $1)'"
 	else
-		git config --global $*
-		echo "Set $*"
+		if git config --global "$1" "$2"; then
+			echo "Set $*"
+		else
+			echo "Failed to set alias: $*"
+			exit_status=1
+		fi
 	fi
 }
 
@@ -14,9 +26,14 @@ set-config user.email mityu.mail@gmail.com
 set-config color.ui true
 set-config core.editor vim
 set-config pull.rebase false
+set-config push.followTags true
 set-config commit.verbose true
 set-config rebase.autoSquash true
 set-config rebase.autoStash true
 set-config fetch.prune true
 set-config diff.algorithm histogram
 set-config github.user mityu
+set-config ghq.root ~/dev
+set-config -f alias.vim '!vim --cmd "autocmd User DenopsPluginPost:gin ++once call feedkeys(\"\<Cmd>GinStatus\<CR>\", \"n\")"'
+
+exit $exit_status
