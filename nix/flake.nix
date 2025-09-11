0317bf94a7b97ad6eeb468ef5950d9aec620f473
mityu <mityu.mail@gmail.com>
2025-09-12 06:13:11 +0900

    nix: Rename nixos module name

--- nix/flake.nix
+++ nix/flake.nix
@@ -72,7 +72,7 @@
       #   inherit username;
       # };
       nixosConfigurations = {
-        myNixOS = inputs.nixpkgs.lib.nixosSystem {
+        laptop-hp-envy-awesomewm = inputs.nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
             ./nixos/configuration.nix
