822a132e8f331eda06b050d9cb6e015c49b1c199
mityu <mityu.mail@gmail.com>
2025-09-21 05:50:34 +0900

    nix: Port xremap configs into Nix

--- nix/nixos/app/xremap.nix
+++ nix/nixos/app/xremap.nix
@@ -1,42 +1,140 @@
-{ pkgs, xremap, platform, username, ... }:
-  if platform.X11 then
-    {
-      imports = [ xremap.nixosModules.default ];
-      services.xremap = {
-        userName = username;
-        serviceMode = "system";
-        withX11 = true;
-        watch = true;
-        yamlConfig = builtins.readFile ../../../xremap/config.yml;
+{ pkgs, lib, xremap, platform, username, ... }:
+  let
+    buildCompleteFeatureFlags = flags:
+      flags // {
+        # Enable "withWlroots" option only when all of the other features are false.
+        withWlroots = lib.lists.all (flag: flag == false) (builtins.attrValues flags);
       };
+    featureFlags = buildCompleteFeatureFlags {
+      withX11 = platform.X11;
+      withNiri = platform.NiriWM;
+    };
+  in
+  {
+    imports = [ xremap.nixosModules.default ];
 
-      systemd.user.services.set-xhost = {
-        description = "Run a one-shot command upon user login";
-        path = [ pkgs.xorg.xhost ];
-        wantedBy = [ "default.target" ];
-        script = "xhost +SI:localuser:root";
-        environment.DISPLAY = ":0.0";
-      };
-    }
-  else if platform.NiriWM then
-    {
-      imports = [ xremap.nixosModules.default ];
-      services.xremap = {
-        userName = username;
-        serviceMode = "user";
-        withNiri = true;
-        watch = true;
-        yamlConfig = builtins.readFile ../../../xremap/config.yml;
-      };
-    }
-  else
-    {
-      imports = [ xremap.nixosModules.default ];
-      services.xremap = {
-        userName = username;
-        serviceMode = "user";
-        withWlroots = true;
-        watch = true;
-        yamlConfig = builtins.readFile ../../../xremap/config.yml;
-      };
-    }
+    services.xremap = featureFlags // {
+      userName = username;
+      serviceMode = if platform.X11 then "system" else "user";
+      watch = true;
+
+      # To check application names, run `wmctrl -x -l`
+      # To see xremap debug logs, run `journalctl -u xremap`
+      config.modmap = [
+        {
+          name = "Global";
+          remap = {
+            "CapsLock" = "CTRL_L";
+            "Super_L" = {
+              held = "Super_L";
+              alone = "Muhenkan";
+              alone_timeout_millis = 750;
+            };
+            "Super_R" = {
+              held = "Super_R";
+              alone = "Henkan";
+              alone_timeout_millis = 750;
+            };
+          };
+        }
+      ];
+
+      config.keymap = [
+        {
+          name = "Firefox";
+          application.only = [ "Navigator.firefox" ];
+          remap = {
+            "C-Shift-q" = "C-Shift-q";
+            "Super-Shift-n" = "C-Shift-q";
+          };
+        }
+        {
+          name = "Browser";
+          application.only = [
+            "firefox"
+            "Navigator.firefox"
+            "Google-chrome"
+            "Chromium"
+            "Vivaldi-stable"
+          ];
+          remap = {
+            "Super-t" = "C-t";
+            "Super-Shift-t" = "C-Shift-t";
+            "Super-n" = "C-n";
+            "Super-Shift-n" = "C-Shift-n";
+            "Super-w" = "C-w";
+            "Super-Shift-w" = "C-Shift-w";
+            "Super-r" = "C-r";
+            "Super-l" = lib.mkIf platform.Xfce "C-l";
+            "Super-f" = "C-f";
+            "Super-d" = "C-d";
+            "Super-y" = "C-h";
+            "Super-LeftBrace" = "C-LeftBrace";
+            "Super-RightBrace" = "C-RightBrace";
+          };
+        }
+        {
+          name = "macOS like";
+          application.not = [
+            "URxvt"
+            "org.wezfurlong.wezterm"
+            "org.wezfurlong.wezterm.org.wezfurlong.wezterm"
+            "gnome-terminal-server.Gnome-terminal"
+            "Alacritty"
+            "Gvim"
+          ];
+          remap = {
+            "C-b" = "Left";
+            "C-f" = "Right";
+            "C-p" = "Up";
+            "C-n" = "Down";
+            "C-h" = "Backspace";
+            "C-d" = "Delete";
+            "C-a" = "Home";
+            "C-e" = "End";
+            "C-m" = "Enter";
+            "C-j" = "Enter";
+            "C-o" = [ "Enter" "Left" ];
+            "Super-c" = "C-c";
+            "Super-v" = "C-v";
+            "Super-a" = "C-a";
+          };
+        }
+        {
+          name = "Xfce4-appfinder";
+          application.only = [ "xfce4-appfinder.Xfce4-appfinder" ];
+          remap = {
+            "Control-Space" = "Esc";
+          };
+        }
+        {
+          name = "Application specific kill";
+          application.only = [ "Slack" "discord" ];
+          remap = {
+            "Super-q" = "C-q";
+          };
+        }
+        {
+          name = "Window manager's kill";
+          remap = {
+            "Super-q" = lib.mkIf platform.X11 {
+              launch = [
+                "${lib.getExe pkgs.xdotool}"
+                "getactivewindow"
+                "windowclose"
+              ];
+            };
+          };
+        }
+      ];
+
+    };
+
+    systemd.user.services.set-xhost = lib.mkIf platform.X11 {
+      description = "Run a one-shot command upon user login";
+      path = [ pkgs.xorg.xhost ];
+      wantedBy = [ "default.target" ];
+      script = "xhost +SI:localuser:root";
+      environment.DISPLAY = ":0.0";
+    };
+  }
