4a5809fbba79b0b7261cc4edf681ebf7d7dfc085
mityu <mityu.mail@gmail.com>
2025-10-16 00:18:31 +0900

    nvim: Use hlchunk to emphasize indent

---
+++ nvim/lua/plugins/hlchunk.lua
@@ -0,0 +1,19 @@
+return {
+  'https://github.com/shellRaining/hlchunk.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    require('hlchunk').setup({
+      chunk = {
+        enable = true,
+        use_treesitter = false,
+        duration = 0,
+        delay = 10,
+        style = '#60aaaa',
+      },
+      indent = {
+        enable = true,
+        delay = 0,
+      },
+    })
+  end,
+}
