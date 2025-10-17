46676aa4619e6c95cfefc999ed4809f35f3284c0
mityu <mityu.mail@gmail.com>
2025-10-17 19:00:16 +0900

    nix: Enable other nvidia modules

--- nix/nixos/pc/desktop-b760m-pro/configuration.nix
+++ nix/nixos/pc/desktop-b760m-pro/configuration.nix
@@ -18,7 +18,12 @@
 
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.open = false;
+  hardware.nvidia = {
+    open = false;
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    nvidiaSettings = true;
+  };
 
   virtualisation.docker.rootless = {
     enable = true;
