df2fa9382f8d9e472470584cb0cc173d24509678
mityu <mityu.mail@gmail.com>
2026-01-07 19:55:47 +0900

    nvim: Add operator-swap

--- nvim/lua/plugins/operator-user.lua
+++ nvim/lua/plugins/operator-user.lua
@@ -7,4 +7,11 @@ return {
     'https://github.com/kana/vim-operator-replace',
     keys = { { 'ms', '<Plug>(operator-replace)', mode = { 'x', 'n', 'o' } } },
   },
+  {
+    'https://github.com/osyo-manga/vim-operator-swap',
+    keys = {
+      { 'my', '<Plug>(operator-swap-marking)', mode = { 'x', 'n', 'o' } },
+      { 'mp', '<Plug>(operator-swap)', mode = { 'x', 'n', 'o' } },
+    },
+  },
 }
