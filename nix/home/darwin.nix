ca05968b63021f8cf5d9269ce1e224c676eda27d
mityu <mityu.mail@gmail.com>
2026-01-20 06:15:33 +0900

    nix: Add softwares for macOS

--- nix/home/darwin.nix
+++ nix/home/darwin.nix
@@ -14,10 +14,20 @@
   };
 
   home.packages = with pkgs; [
+    # cabextract
+    darwin.trash
     gnupg
+    git
+    translate-shell
     (import ./pkgs/ott.nix {
       inherit (inputs) opam-nix;
       inherit (pkgs.stdenv.hostPlatform) system;
     })
   ];
+
+  programs.gpg.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    pinentry.package = pkgs.pinentry_mac;
+  };
 }
