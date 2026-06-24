7481f2fcef48f0c331301bc7f4f6cbdf71149ee3
mityu <mityu.mail@gmail.com>
2026-06-24 23:46:10 +0900

    nix: Fix startup failure of XFCE4

--- nix/nixos/de/xfce.nix
+++ nix/nixos/de/xfce.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   imports = [
     ../app/xremap.nix
@@ -26,4 +26,14 @@
     thunar-volman
     thunar-archive-plugin
   ];
+
+  environment.pathsToLink = lib.warn ''
+    Adding `/etc/xdg` to `environment.pathsToLink` as a workaround to deal with startup failure of XFCE4.
+    It maybe due to a break change that stops linking the `/etc/xdg` directory automatically.
+    The change is introduced in: https://github.com/NixOS/nixpkgs/pull/530382
+    And maybe related (may reverts the above): https://github.com/NixOS/nixpkgs/pull/533416
+
+    It is left for a future task that what should be actually linked.
+    Notes for the future: xfce4-session package have /etc/xdg files.
+  '' [ "/etc/xdg" ];
 }
