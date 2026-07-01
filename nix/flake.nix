aba9f482c059a4c2d395be7cc078980e04afa5e9
mityu <mityu.mail@gmail.com>
2026-07-01 21:35:21 +0900

    nix: Use nixos-26.05 branch to get stable packages

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     home-manager = {
       url = "github:nix-community/home-manager";
