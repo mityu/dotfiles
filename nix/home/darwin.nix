f4d16bf0d6543d49b38a8318aed081311e316f5e
mityu <mityu.mail@gmail.com>
2026-07-07 07:34:13 +0900

    nix: Fix build failure of satysfi on macOS

--- nix/home/darwin.nix
+++ nix/home/darwin.nix
@@ -1,7 +1,7 @@
 {
   pkgs,
+  lib,
   username,
-  inputs,
   ...
 }:
 {
@@ -13,6 +13,22 @@
     homeDirectory = "/Users/${username}";
     stateVersion = "26.05";
   };
+
+  nixpkgs.overlays = [
+    # Fix ocamlPackages.camlimages package, which is required by satysfi.
+    (
+      final: prev:
+      let
+        ocamlPackages = "ocamlPackages_4_14";
+      in
+      lib.recursiveUpdate prev {
+        ocaml-ng.${ocamlPackages}.camlimages = prev.ocaml-ng.${ocamlPackages}.camlimages.overrideAttrs (o: {
+          # Remove the lablgtk package, which causes the build failure on macOS.
+          buildInputs = (builtins.filter (p: !(lib.hasInfix "lablgtk" p.name)) o.buildInputs);
+        });
+      }
+    )
+  ];
 
   home.packages = with pkgs; [
     # cabextract
