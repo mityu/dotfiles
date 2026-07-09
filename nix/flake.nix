44ec14449cd4025d237392509e1c501b30d31e9a
mityu <mityu.mail@gmail.com>
2026-07-09 23:10:32 +0900

    nix: Use the `nixos-unstable` branch instead of `nixpkgs-unstable` branch

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
 
